786c3dc29ef6b5247a2da1441d416cad620bf1bd
Sorrow <sanjuan.thibaut@gmail.com>
2023-01-11 21:02:12 +0100

    feat(script): Add functionality to clear console messages from local storage

--- lacalvbattlelogs.user.js
+++ lacalvbattlelogs.user.js
@@ -3,7 +3,7 @@
 // @author      Sorrow
 // @description Ce script intercepte les réponses et les affiches dans la console LaCalv Battle Log, parsée et formatée de manière à être facilement lisible.
 // @include     https://lacalv.fr/
-// @version     0.5
+// @version     0.6
 
 // @homepageURL   https://github.com/sanjuant/LaCalvBattleLogs/
 // @supportURL    https://github.com/sanjuant/LaCalvBattleLogs/issues
@@ -18,7 +18,7 @@ class Execution {
     static worldboss_reward_printed = null;
     static admin_reward = null;
     static admin_reward_printed = null;
-    static wbclassement = { top: [], user: { classement: 0, dommages: 0 } };
+    static wbclassement = {top: [], user: {classement: 0, dommages: 0}};
 }
 
 class Update {
@@ -28,14 +28,14 @@ class Update {
 }
 
 realProcess = function (xhr) {
-    if (xhr.responseURL === "https://lacalv.fr/play/battlewb") {
+    if (xhr.responseURL === "https://lacalv.fr/play/update") {
+        parseUpdateResponse(xhr.response)
+    }else if (xhr.responseURL === "https://lacalv.fr/play/battlewb") {
         parseBattleWbResponse(xhr.response)
     } else if (xhr.responseURL.startsWith("https://lacalv.fr/play/battle?opponent")) {
         parseBattleOpponentResponse(xhr.response)
     } else if (xhr.responseURL.startsWith("https://lacalv.fr/play/battlepve")) {
         parseBattlePveResponse(xhr.response)
-    } else if (xhr.responseURL === "https://lacalv.fr/play/update") {
-        parseUpdateResponse(xhr.response)
     } else if (xhr.responseURL === "https://lacalv.fr/play/wbclassement") {
         parseWbClassementResponse(xhr.response)
     }
@@ -54,7 +54,7 @@ function parseUpdateResponse(response) {
 function parseBattleWbResponse(response) {
     const data = JSON.parse(response)
     const {player, opponent} = getStatsFromBattle(data);
-    appendToConsole(`Boss: Vous avez causé ${player.dmg} dommages.`);
+    appendToConsole(`Boss&nbsp;: Vous avez causé ${player.dmg} dommages.`);
 }
 
 function parseWbClassementResponse(response) {
@@ -73,23 +73,31 @@ function parseBattlePveResponse(response) {
         }
     }
 
-    const infosWin = [('alopieces' in data.rewards && data.rewards.alopieces > 0) ? `Alopièces: ${data.rewards.alopieces}` : '', ('event' in data.rewards && data.rewards.event > 0) ? `Event: ${data.rewards.event}` : '', ('expUser' in data && data.expUser > 0) ? `Expérience: ${data.expUser}` : '', (itemsWin.length > 0) ? `Items: ${itemsWin.join(', ')}` : ''];
+    const infosWin = [('alopieces' in data.rewards && data.rewards.alopieces > 0) ? `Alopièces&nbsp;:&nbsp;${data.rewards.alopieces}` : '', ('event' in data.rewards && data.rewards.event > 0) ? `Events&nbsp;:&nbsp;${data.rewards.event}` : '', ('expUser' in data && data.expUser > 0) ? `Expériences&nbsp;:&nbsp;${data.expUser}` : '', (itemsWin.length > 0) ? `Items&nbsp;:&nbsp;${itemsWin.join(', ')}` : ''];
 
-    const infosLose = [(opponent.soinTotal > 0 || opponent.pvRecover !== -1) ? `Soins: ${opponent.soinTotal}` : '', (opponent._bouclier > 0) ? `Bouclier: ${opponent.bouclier}` : '', `Vie: ${opponent.pvFinal}`];
+    const infosLose = [(opponent.soinTotal > 0 || opponent.pvRecover !== -1) ? `Soins&nbsp;:&nbsp;${opponent.soinTotal}` : '', (opponent._bouclier > 0) ? `Bouclier&nbsp;:&nbsp;${opponent.bouclier}` : '', `Vie&nbsp;:&nbsp;${opponent.pvFinal}`];
 
     if (player.result === 'winner') {
-        appendToConsole(`ToB: Vous avez gagné. ${infosWin.filter(Boolean).join(' - ')}`);
+        appendToConsole(`ToB&nbsp;: Vous avez gagné. ${infosWin.filter(Boolean).join(', ')}`);
     } else {
-        appendToConsole(`ToB: Vous avez perdu. ${infosLose.filter(Boolean).join(' - ')}`);
+        appendToConsole(`ToB&nbsp;: Vous avez perdu. ${infosLose.filter(Boolean).join(', ')}`);
     }
 }
 
 function parseBattleOpponentResponse(response) {
     const data = JSON.parse(response)
     const {player, opponent} = getStatsFromBattle(data);
-    const infos = [(data.eloUser > 0) ? `Elo: ${(player.result === 'looser') ? '-' : ''}${data.eloUser}` : '', (data.aloUser > 0) ? `Alopièces: ${data.aloUser}` : '', (data.event > 0) ? `Event: ${data.event}` : '', (data.expUser > 0) ? `Expérience: ${data.expUser}` : '',];
-    const infosLose = [(opponent.soinTotal > 0 || opponent.pvRecover !== -1) ? `Soins: ${opponent.soinTotal}` : '', (opponent._bouclier > 0) ? `Bouclier: ${opponent.bouclier}` : '', `Vie: ${opponent.pvFinal}`];
-    appendToConsole(`PvP: Vous avez ${player.result === 'winner' ? "gagné" : "perdu"}, contre ${opponent.name}. ${infos.filter(Boolean).join(' - ')}${player.result === 'looser' ? infosLose.filter(Boolean).join(' - ') : ''}`);
+    const infos = [
+        [ (data.eloUser > 0) ? `Elo&nbsp;:&nbsp;${(player.result === 'looser') ? '-' : ''}${data.eloUser}` : '',
+        (data.aloUser > 0) ? `Alopièces&nbsp;:&nbsp;${data.aloUser}` : '', (data.event > 0) ? `Event: ${data.event}` : '',
+        (data.expUser > 0) ? `Expérience&nbsp;:&nbsp;${data.expUser}` : ''
+        ].filter(Boolean).join(', '),
+        [(player.result === 'looser' && (opponent.soinTotal > 0 || opponent.pvRecover !== -1)) ? `Soins&nbsp;:&nbsp;${opponent.soinTotal}` : '',
+        (player.result === 'looser' && opponent._bouclier > 0) ? `Bouclier&nbsp;:&nbsp;${opponent.bouclier}` : '',
+        (player.result === 'looser') ? `Vie&nbsp;:&nbsp;${opponent.pvFinal}` : ''].filter(Boolean).join(', ')
+    ];
+
+    appendToConsole(`PvP&nbsp;: Vous avez ${player.result === 'winner' ? "gagné" : "perdu"}, contre ${opponent.name}. ${infos.filter(Boolean).join(' - ')}`);
 }
 
 function printNotifs() {
@@ -98,17 +106,17 @@ function printNotifs() {
     Execution.admin_reward = reward.admin
 
     if (null != Execution.worldboss_reward && (Execution.worldboss_reward_printed === null || Execution.worldboss_reward.date !== Execution.worldboss_reward_printed.date)) {
-        appendToConsole(`Combat Boss: ${Execution.worldboss_reward.text}`)
+        appendToConsole(`Combat Boss&nbsp;: ${Execution.worldboss_reward.text}`)
         Execution.worldboss_reward_printed = Execution.worldboss_reward
     } else if (null != Execution.admin_reward && (Execution.admin_reward_printed === null || Execution.admin_reward.date !== Execution.admin_reward_printed.date)) {
-        appendToConsole(`Admin: ${Execution.admin_reward.text}`)
+        appendToConsole(`Admin&nbsp;: ${Execution.admin_reward.text}`)
         Execution.admin_reward_printed = Execution.admin_reward
     }
 }
 
 function printWbclassement() {
-    if (Update.wb < -1 && Execution.wbclassement.top.length > 0) {
-        let msg = "Classement Worldboss:"
+    if (Update.wb < 0 && Execution.wbclassement.top.length > 0) {
+        let msg = "Classement Worldboss&nbsp;:"
         for (let i = 0; i < Execution.wbclassement['top'].length; i++) {
             let user = Execution.wbclassement['top'][i];
             msg += "\n";
@@ -123,13 +131,13 @@ function printWbclassement() {
 }
 
 
-setInterval(function() {
+setInterval(function () {
     printNotifs()
     printWbclassement()
 }, 15000);
 
 function getRewardsInNotifs(notifs) {
-    let rewards = { admin: null, worldboss: null };
+    let rewards = {admin: null, worldboss: null};
     for (let i = 0; i < notifs.length; i++) {
         let notif = notifs[i];
         if (notif.type === 'Boss des Mondes') {
@@ -300,8 +308,8 @@ function appendToConsole(text, save = true) {
     let consoleMessages = JSON.parse(localStorage.getItem('consoleMessages'))
     consoleMessages.push(message);
 
-    const preEl = document.createElement('pre');
-    preEl.textContent = `[${message.time}] ${message.text}`;
+    const preEl = document.createElement('p');
+    preEl.innerHTML = `[${message.time}] ${message.text}`;
     msgEl.appendChild(preEl);
     msgEl.scrollTop = msgEl.scrollHeight;
 
@@ -330,6 +338,7 @@ expandButtonEl.classList.add("expand-button-element")
 console.log(localStorage.getItem('expanded'))
 if (JSON.parse(localStorage.getItem('expanded')) === true) {
     expandButtonEl.textContent = '-';
+    consoleEl.style.height = '100%';
 } else {
     expandButtonEl.textContent = '+';
     consoleEl.style.height = '36px';
@@ -338,8 +347,16 @@ if (JSON.parse(localStorage.getItem('expanded')) === true) {
 const msgEl = document.createElement('div');
 msgEl.classList.add("message-element")
 
+const clearButtonEl = document.createElement('button');
+clearButtonEl.innerHTML = '&nbsp;&nbsp;&nbsp;<svg xmlns="http://www.w3.org/2000/svg" fill="none" width="18" height="18" viewBox="0 0 24 24" stroke-width="1.5" stroke="#fff" ><path stroke-linecap="round" stroke-linejoin="round" d="M14.74 9l-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 01-2.244 2.077H8.084a2.25 2.25 0 01-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 00-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 013.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 00-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 00-7.5 0" /></svg>';
+clearButtonEl.addEventListener("click", () => {
+    msgEl.innerHTML = "";
+    localStorage.setItem('consoleMessages', JSON.stringify([]));
+});
+
 // Add an event listener to toggle the height of the console
 expandButtonEl.addEventListener('click', () => {
+    console.log(consoleEl.style.height)
     if (consoleEl.style.height === '100%') {
         consoleEl.style.height = '36px';
         expandButtonEl.textContent = '+';
@@ -353,6 +370,7 @@ expandButtonEl.addEventListener('click', () => {
 
 // Append the title and expand/collapse button to the header
 headerEl.appendChild(titleEl);
+titleEl.appendChild(clearButtonEl);
 headerEl.appendChild(expandButtonEl);
 
 // Append the header to the console
@@ -365,8 +383,8 @@ if (null === localStorage.getItem('consoleMessages')) {
 
 const messages = JSON.parse(localStorage.getItem('consoleMessages'));
 messages.forEach(message => {
-    const preEl = document.createElement('pre');
-    preEl.textContent = `[${message.time}] ${message.text}`;
+    const preEl = document.createElement('p');
+    preEl.innerHTML = `[${message.time}] ${message.text}`;
     msgEl.appendChild(preEl);
     msgEl.scrollTop = msgEl.scrollHeight;
 })
@@ -380,11 +398,14 @@ addGlobalStyle('.title-element{font-weight:700}');
 addGlobalStyle('.expand-button-element{background:0 0;color:#fff;border:none;cursor:pointer}');
 addGlobalStyle('.message-element {height:calc(98% - 36px);overflow:auto}');
 addGlobalStyle('.message-element > pre {margin:0;font-family:monospace;font-size:13px;line-height:18px;padding:0.2rem 0.5rem;white-space:pre-wrap;word-break:break-word;border-bottom:1px solid #ccc;--darkreader-inline-border-bottom:#3e4446}');
+addGlobalStyle('.message-element > p {margin:0;font-family:monospace;font-size:13px;line-height:18px;padding:0.2rem 0.5rem;white-space:pre-wrap;word-break:break-word;border-bottom:1px solid #ccc;--darkreader-inline-border-bottom:#3e4446}');
 
 function addGlobalStyle(css) {
     var head, style;
     head = document.getElementsByTagName('head')[0];
-    if (!head) { return; }
+    if (!head) {
+        return;
+    }
     style = document.createElement('style');
     style.type = 'text/css';
     style.innerHTML = css;
