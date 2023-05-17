7d24c4878c096ff777183f8c34264b4ed2e66450
Sorrow <sanjuan.thibaut@gmail.com>
2023-05-17 12:34:33 +0200

    feat(video): add button to display video

--- src/ComponentLoader.js
+++ src/ComponentLoader.js
@@ -47,6 +47,7 @@ class BattleLogsComponentLoader
         this.__addScript("src/lib/Csv.js");
         this.__addScript("src/lib/Sound.js");
         this.__addScript("src/lib/Battlewbtry.js");
+        this.__addScript("src/lib/Video.js");
 
         this.__loadingOrder += 1;
         this.__addScript("src/BattleLogs.js");
