f36b4136744e7134ab0cfe58ee97c41223ca3615
Sorrow <sanjuan.thibaut@gmail.com>
2023-10-17 22:54:02 +0200

    feat(tests): add __internal__updateAttribute test

--- tests/Battle.test.js
+++ tests/Battle.test.js
@@ -5,3 +5,12 @@ test('should initialize battle settings', () => {
     BattleLogsBattle.initialize();
     expect(BattleLogsBattle.Settings).toBeDefined();
 });
+
+test('__internal__updateAttribute updates the attribute correctly', () => {
+    const user = { name: 'User', famName: 'UserFam', tour: 0 };
+    const opponent = { name: 'Opponent', famName: 'OpponentFam', dmg: 80 };
+    BattleLogsBattle.__internal__updateAttribute('User', user, opponent, 'tour', 1);
+    BattleLogsBattle.__internal__updateAttribute('Opponent', user, opponent, 'dmg', 1000);
+    expect(user.tour).toBe(1);
+    expect(opponent.dmg).toBe(1080);
+});
