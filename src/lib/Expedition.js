67209424fe2bc41d1eb6f7f0257b4dec85da18f4
Baltimousse <daubie_vic@msn.com>
2023-12-06 14:55:36 +0100

    fix(Expedition): add event checker to reward

--- src/lib/Expedition.js
+++ src/lib/Expedition.js
@@ -194,7 +194,11 @@ class BattleLogsExpedition {
             let name;
             let count;
             let rarity;
-            if (!isNaN(parseInt(object["value"], 10))) {
+            if (type === "event") {
+                name = type
+                count = object["count"]
+                rarity = object["rarity"] ? object["rarity"] : 4
+            } else if (!isNaN(parseInt(object["value"], 10))) {
                 name = type === "alopiece" ? "Alopièce" : object["value"]
                 count = object["value"]
                 rarity = object["rarity"] ? object["rarity"] : 4
